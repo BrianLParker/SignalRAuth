7bbf5fdacdc22dc65e42ee2390a6d58eb7bcb14c
Brian Parker <brian.parker@hotmail.com.au>
2020-07-28 20:06:17 +1000

    Add Signalr with Auth

---
+++ SignalRAuth/Server/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalRAuth.Server.Hubs
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.SignalR;
+    using System.Threading.Tasks;
+
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
