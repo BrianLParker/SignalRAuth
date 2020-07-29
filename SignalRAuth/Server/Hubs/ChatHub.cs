d1a85ee5abb31527719e74a7c86f89e4ad2de0a9
Brian Parker <brian.parker@hotmail.com.au>
2020-07-29 22:26:45 +1000

    Expose ApplicationUser in hub

--- SignalRAuth/Server/Hubs/ChatHub.cs
+++ SignalRAuth/Server/Hubs/ChatHub.cs
@@ -1,15 +1,27 @@
 ﻿namespace SignalRAuth.Server.Hubs
 {
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.SignalR;
+    using SignalRAuth.Server.Models;
     using System.Threading.Tasks;
 
     [Authorize]
     public class ChatHub : Hub
     {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ChatHub(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            var userId = Context.UserIdentifier;
+            var applicationUser = await userManager.FindByIdAsync(userId);
+
+            await Clients.All.SendAsync("ReceiveMessage", applicationUser.Email, message);
         }
     }
 }
